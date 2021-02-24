4813d947f7031ffca753642a432e35b13a3e7c6e
Nicolas Gallego <gallego.nicolaspro@gmail.com>
2021-02-24 13:29:36 +0100

    List pretty much done

---
+++ jour3/list/components/List.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { SvgUri } from "react-native-svg";
+
+export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(`http://restcountries.eu/rest/v2/all`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        this.setState({ countries: response });
+      });
+  }
+  render() {
+    return (
+      <View>
+        <FlatList
+          data={this.state.countries}
+          renderItem={({ item }) => (
+            <>
+              <Text style={styles.liste}>
+                {item.name} - {item.capital}
+              </Text>
+              <SvgUri style={styles.img} uri={`${item.flag}`} />
+            </>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  liste: {
+    marginTop: 32,
+    marginBottom: 8,
+  },
+  img: {
+      flex: 1,
+      alignItems: "center", 
+      justifyContent: "center",
+      width: null,
+      height: null,
+  }
+})
